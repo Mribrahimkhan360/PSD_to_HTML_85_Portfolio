28ad7132dd3df85fdc7a8fa8a2fcc15673139323
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2021-06-30 12:32:36 +0600

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
-        600:{
+        200:{
             items:1
         },
         1000:{
@@ -41,4 +41,54 @@ counters.forEach((counter) => {
 		}
 	};
 	updateCounter();
+});
+
+$(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){ 
+    selectedClass = $(this).attr("data-rel"); 
+     $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300); 
+    
+  });
+  });
+  
+  $(document).ready(function(){
+  $('.toolbar button').click(function(){
+    $('button').removeClass("active-portfolio");
+    $(this).addClass("active-portfolio");
+  });
+  });
+
+  $(document).ready(function(){
+  $("#heading").click(function(){
+    $("#problem-toggle").toggle(200);
+  });
+});
+
+
+$(document).ready(function(){
+    $("#management").click(function(){
+      $("#management-toggle").toggle(200);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#inverment").click(function(){
+      $("#inverment-toggle").toggle(200);
+    });
+  });
+
+
+// progress
+
+$('.skill-per').each(function(){
+  var $this = $(this);
+  var per =$this.attr('per');
+  $this.css("width",per+"%");
+  
 });
